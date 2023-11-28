06f442c912a8c8e6da1ece22c66d653785e60dc9
Krithik Singh <krithiksingh@gmail.com>
2023-11-28 09:22:40 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your Circle class here
+class Circle {
+    #radius;
+    #pi;
+    constructor(radius) {
+        this.#radius = radius;
+        this.#pi = Math.PI;
+    }
+
+    get diameter() {
+        return this.#radius * 2;
+    }
+
+    get circumference() {
+        return this.#pi * this.diameter;
+    }
+
+    get area() {
+        return this.#pi * this.#radius ** 2;
+    }
+
+    set diameter(newDiameter) {
+        this.#radius = newDiameter / 2;
+    }
+
+    set circumference(newCircumference) {
+        this.#radius = newCircumference / (2 * this.#pi);
+    }
+
+    set area(newArea) {
+        this.#radius = Math.sqrt(newArea / this.#pi);
+    }
+}
