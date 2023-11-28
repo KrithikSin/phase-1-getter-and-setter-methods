8499f490d71a5ccec689ba35fc23f2b6b1ae0a53
Krithik Singh <krithiksingh@gmail.com>
2023-11-28 09:27:55 -0500

    lab completed getter and setter

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 // Add your Circle class here
 class Circle {
-    #radius;
-    #pi;
     constructor(radius) {
-        this.#radius = radius;
-        this.#pi = Math.PI;
+        this.radius = radius;
+        this.pi = Math.PI;
     }
 
     get diameter() {
-        return this.#radius * 2;
+        return this.radius * 2;
     }
 
     get circumference() {
-        return this.#pi * this.diameter;
+        return this.pi * this.diameter;
     }
 
     get area() {
-        return this.#pi * this.#radius ** 2;
+        return this.pi * this.radius ** 2;
     }
 
     set diameter(newDiameter) {
-        this.#radius = newDiameter / 2;
+        this.radius = newDiameter / 2;
     }
 
     set circumference(newCircumference) {
-        this.#radius = newCircumference / (2 * this.#pi);
+        this.radius = newCircumference / (2 * this.pi);
     }
 
     set area(newArea) {
-        this.#radius = Math.sqrt(newArea / this.#pi);
+        this.radius = Math.sqrt(newArea / this.pi);
     }
 }
